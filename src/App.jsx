a4164f19d7b36998415ec5e88264ebeaf083d2ce
henrynguyen1 <38772791+henrynguyen1@users.noreply.github.com>
2023-01-02 19:48:32 +1100

    added routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
-import Header from '';
+import Header from './global/Header';
+import Footer from './global/Footer';
+import { BrowserRouter as Router ,
+    Switch,
+    Route } from 'react-router-dom';
 
 
 class App extends React.Component {
 
 // Research state managment components
+render(){
+
+    return (
+        <div>
+    <Router>
+    <routes>
+       
+
+    </routes>
+    </Router>
+    </div>
+    
+    )
+}
+
+
     
 }
 export default App;
