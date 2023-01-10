3035aac8d6ebe49b76ce1c1d9172ebf30983a7fd
henrynguyen1 <38772791+henrynguyen1@users.noreply.github.com>
2023-01-10 17:20:45 +1100

    added to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,60 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
-import Header from './global/Header';
-import Footer from './global/Footer';
-import { BrowserRouter as Router ,
-    Switch,
-    Route } from 'react-router-dom';
-
+import React from "react";
+import Header from "./global/Header";
+import Footer from "./global/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
-
-// Research state managment components
-render(){
-    return (    
-    <Router>
+  // Research state managment components
+  render() {
+    return (
+      <Router>
         <div className="app">
-            <div className="headerContainer">
-            <Route exact path="/" render={props => (
-                                <React.Fragment>
-                                <div>
-                                   <div className="center">
-                                        <Header/>
-                                   </div>
-                                </div>
-                                </React.Fragment>
-                            )} />
-            </div>
-    <routes>
-       
-
-    </routes>
-
-    </div>
-    </Router>
-    
-    
-    )
-}
-
-
-    
+          <div className="appContainer">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <div>
+                    <div className="center"></div>
+                  </div>
+                </React.Fragment>
+              )}
+            />
+            <Route
+              exact
+              path="/balance"
+              render={(props) => (
+                <React.Fragment>
+                  <BalanceManagement />
+                </React.Fragment>
+              )}
+            />
+            <Route
+              exact
+              path="/account"
+              render={(props) => (
+                <React.Fragment>
+                  <AccountDetails />
+                </React.Fragment>
+              )}
+            />
+            <Route
+              exact
+              path="/topup"
+              render={(props) => (
+                <React.Fragment>
+                  <Topup />
+                </React.Fragment>
+              )}
+            />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 export default App;
--- src/global/Header.jsx
+++ src/global/Header.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import RomanFlag from '../images/1920px-Nova_Roma_Flag.svg.png'
-import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Nav, Navbar, NavDropdown}from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Header extends React.Component {
@@ -12,25 +10,21 @@ class Header extends React.Component {
        return (
         <><div className='headerImage'>
              <img src={RomanFlag} alt="logo" />
-            </div><div className='headerMenu'>
+            </div>
+            <div className='headerMenu'>
                     <Navbar bg="light" expand="lg">
                         <Container>
                             <Navbar.Brand href="#home">Imperium Romanum</Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
                                 <Nav className="me-auto">
-                                    <Nav.Link href="#home"></Nav.Link>
-                                    <Nav.Link href="#link">Link</Nav.Link>
+                                    <Nav.Link href="#home" to=''></Nav.Link>
+                                    <Nav.Link href="#link">Timeline</Nav.Link>
                                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.2">
-                                            Another action
-                                        </NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action/3.4">
-                                            Separated link
-                                        </NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.1" to=''>Kingdom</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2" to=''>Republic</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.3" to=''>Imperial</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.4" to=''>Byzantine</NavDropdown.Item>
                                     </NavDropdown>
                                 </Nav>
                             </Navbar.Collapse>
@@ -40,7 +34,5 @@ class Header extends React.Component {
         );
     }
 }
-
-
 
 export default Header;
